90e4a5192a434e1320b4cc55f3cd78a21f63558e
Ananthu Kumar A N <ananthunalloor@gmail.com>
2025-10-13 12:04:09 +0530

    package

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ from kivy_garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
-plt.plot([1, 23, 2, 4])
-plt.ylabel("some numbers")
-
 
 class MyApp(App):
     def build(self):
         box = BoxLayout()
+
         box.add_widget(FigureCanvasKivyAgg(plt.gcf()))
         return box
 
 
 def main():
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="3d")
+
+    ax.set(xlim3d=(0, 1), xlabel="X")
+    ax.set(ylim3d=(0, 1), ylabel="Y")
+    ax.set(zlim3d=(0, 1), zlabel="Z")
     MyApp().run()
 
 
